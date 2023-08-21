dcc621ffb5cf492232455399ede1bdb95fce7543
Jerzke <jerekarvonen1@gmail.com>
2023-08-21 16:40:38 +0300

    Created category picker and textinput screens

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import AppTextInput from "./app/components/AppTextInput";
+
+import colors from "./app/config/colors";
+import AppPicker from "./app/components/AppPicker";
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
 
 export default function App() {
-  return <AccountScreen />;
+  const [category, setCategory] = useState(categories[0]);
+
+  return (
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
+  );
 }
--- app/config/colors.js
+++ app/config/colors.js
@@ -3,6 +3,8 @@ export default {
     secondary: "#4ecdc4",
     black: "#171717",
     white: '#fff',
-    medium: "#6e6969"
+    medium: "#6e6969",
+    light: "#f8f4f4",
+    dark:"#0c0c0c"
 
 };
